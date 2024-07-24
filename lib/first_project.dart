6e35316fda187ae99552effd3f1d78db22f9ec8f
alyfadel <alyfadel67@gmail.com>
2024-07-24 18:31:48 +0300

    finish login screen

---
+++ lib/first_project.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+
+class FirstProject extends StatelessWidget {
+  const FirstProject({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.teal,
+        leading: const Icon(
+          Icons.menu,
+          color: Colors.white,
+          size: 35.0,
+        ),
+        title: const Text(
+          'First Project',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 25.0,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        actions: [
+          const IconButton(
+              onPressed: onClicked,
+              icon: Icon(
+                Icons.notifications,
+                color: Colors.white,
+              )),
+          IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.search,
+                color: Colors.white,
+              )),
+        ],
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(50.0),
+            child: Stack(
+              alignment: AlignmentDirectional.bottomCenter,
+              children: [
+                Container(
+                  width: 250.0,
+                  height: 250.0,
+                  child:  const Image(
+                    fit: BoxFit.cover,
+                    image: NetworkImage(
+                      'https://static.pexels.com/photos/36753/flower-purple-lical-blosso.jpg',
+                    ),
+                  ),
+                ),
+                Container(
+                  width: 250,
+                  margin: EdgeInsets.symmetric(
+                      vertical: 20.0
+                  ),
+                  color: Colors.black45,
+                  child: const Text(
+                    'Flowers',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 40.0,
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+void onClicked() {
+  print('click');
+}
---
+++ lib/login_screen.dart
@@ -0,0 +1,132 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+var emailController = TextEditingController();
+var passwordController = TextEditingController();
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Padding(
+        padding: const EdgeInsets.all(
+          20.0,
+        ),
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  'Login',
+                  style: TextStyle(
+                    fontSize: 40.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(
+                  height: 20.0,
+                ),
+                const Text(
+                  'Login to browse hot offer',
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    color: Colors.grey,
+                  ),
+                ),
+                const SizedBox(
+                  height: 20.0,
+                ),
+                TextFormField(
+                  controller: emailController,
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(
+                        10.0,
+                      ),
+                    ),
+                    label: Text('Email Address'.toUpperCase()),
+                    prefixIcon: const Icon(
+                      Icons.email_outlined,
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 20.0,
+                ),
+                TextFormField(
+                  controller: passwordController,
+                  keyboardType: TextInputType.datetime,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(
+                        10.0,
+                      ),
+                    ),
+                    label: Text(
+                      'password'.toUpperCase(),
+                    ),
+                    prefixIcon: const Icon(
+                      Icons.lock_outlined,
+                    ),
+                    suffixIcon: const Icon(
+                      Icons.visibility_off_outlined,
+                    ),
+                  ),
+                  obscureText: true,
+                ),
+                const SizedBox(
+                  height: 20.0,
+                ),
+                Container(
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20.0),
+                    color: Colors.blue,
+                  ),
+                  child: MaterialButton(
+                    onPressed: () {},
+                    child: const Text(
+                      'Login',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 25.0,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 300.0,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      'Don\'t Have Account ?!',
+                      style: TextStyle(
+                          fontSize: 15.0, fontWeight: FontWeight.bold),
+                    ),
+                    TextButton(
+                      onPressed: () {},
+                      child: const Text(
+                        'Register',
+                        style: TextStyle(
+                          fontSize: 25.0,
+                          color: Colors.red,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
